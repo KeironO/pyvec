c5cfbf7190ad716de3c8eb0d59e637f3148d6606
Keiron O'Shea <keiron.oshea@gmail.com>
2015-08-11 02:57:58 +0300

    Added some comments and did a little housekeeping

--- pyvec/dataset.py
+++ pyvec/dataset.py
@@ -2,18 +2,39 @@ import os, csv
 import numpy as np
 from PIL import Image
 
-pictures_directory = ""
+'''
+    file name: dataset.py
+    author/s: Keiron O'Shea
+    description: simple toolkit to load images, with their adjacent labels
+    into arrays - suitable for use with keras/theano.
+'''
 
+'''
+get_labels()
+
+    Allows the correlation of labels to its image for pre-labelling purposes.
+
+    labels_file must be in the following format:
+        image name\tlabel
+        example\t0
+        example2\t1
+'''
 def get_labels(labels_file):
     table={}
     with open(labels_file, "rb") as labels_reader:
         labels_reader = csv.reader(labels_file, delimiter="\t", lineterminator="\n")
-        labels_reader.next()
-
+        labels_reader.next() #Skip the heading line
         for labels in labels_reader:
             table[labels[0]] = int(labels[1])
         return table
 
+'''
+load_images()
+
+    Loads the preprocessed images from a folder, and correlates them
+    with a correct label for use in keras/theano.
+
+'''
 def load_images(directory, image_height, image_width):
     images = os.listdir(directory)
     number_images = len(images)
