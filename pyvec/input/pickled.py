99b6819a55257a47b3d3f81376e3410c0d377430
NJD <nid16@aber.ac.u>
2015-08-17 22:21:40 +0100

    Fixed output and added import for pickle

---
+++ pyvec/input/pickled.py
@@ -0,0 +1,6 @@
+import cPickle as pickle
+
+def pickle_in(directory, train_data, train_labels, val_data, val_labels, test_data=None, test_labels=None):
+    train_data, train_labels, val_data, val_labels, test_data, test_labels = pickle.load(open(directory+"/pdata.p", 'rb'))
+   
+  
--- pyvec/output/pickles.py
+++ pyvec/output/pickles.py
@@ -1,7 +1,5 @@
 import cPickle as pickle
 
 def pickle_data(directory, train_data, train_labels, val_data, val_labels, test_data=None, test_labels=None):
-    print directory
-    out = file(directory+"/pdata.p", 'wb')
-    pickle.dump((train_data, train_labels, val_data, val_labels, test_data, test_labels), out, protocol=pickle.HIGHEST_PROTOCOL)
-    out.close()
+    pickle.dump((train_data, train_labels, val_data, val_labels, test_data, test_labels), open(directory+"/pdata.p", 'wb'), protocol=pickle.HIGHEST_PROTOCOL)
+
