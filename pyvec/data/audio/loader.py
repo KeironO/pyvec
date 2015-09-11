929440ebab6bc76a1268621220c49f1d06d86a9a
KeironO <keiron.oshea@gmail.com>
2015-09-11 14:07:36 +0300

    Updated README

--- pyvec/data/audio/loader.py
+++ pyvec/data/audio/loader.py
@@ -1,8 +1,21 @@
 import scipy.io.wavfile as wavfile
-import numpy
+import numpy, os.walk
+
+def get_labels(directory):
+    audlist = []
+    for dir_name, dir_names, file_name in os.walk(directory):
+        for file in file_name:
+            label = os.path.basename(os.path.normpath(dir_name))
+            audlist.append([label, file])
+    return audlist
 
 
-def vectorise_audio(file):
+def get_max_length(directory):
+    return 0
+    # ToDo
+
+def vectorise_audio(directory, length):
+    audlist = get_labels(directory)
     read_file = wavfile.read(file)
     sound_vector = numpy.array(read_file[1], dtype="float32")
     return sound_vector
